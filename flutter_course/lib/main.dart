2008cc75fa2d0f47be1b8daf412d525acb6e634f
Joey Fallone <j.fallone@ualberta.ca>
2019-12-23 22:04:45 -0700

    Style improvements

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(CourseApp());
-}
+void main() => runApp(CourseApp());
 
 class CourseApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(appBar: AppBar(title: Text('Joey\'s List App'))));
