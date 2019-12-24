a5a0468ef9a2404cac47b5e1138e7bd4750dde6a
Joey Fallone <j.fallone@ualberta.ca>
2019-12-23 22:20:53 -0700

    Add functionless button

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -6,12 +6,30 @@ class CourseApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(title: Text('Joey\'s List App')),
-            body: Card(
-                child: Column(children: <Widget>[
-              Image.asset("assets/images/scarlet_spider.png"),
-              Text("Scarlet Spider")
-            ]))));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Joey\'s Superhero List App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text("Add Superhero"),
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset("assets/images/scarlet_spider.png"),
+                  Text("Scarlet Spider")
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
