07f9fb80f375a30e788ee35858e18e859a881a15
Joey Fallone <j.fallone@ualberta.ca>
2019-12-23 22:14:14 -0700

    Add a cool superhero picture :P

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -6,6 +6,12 @@ class CourseApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(appBar: AppBar(title: Text('Joey\'s List App'))));
+        home: Scaffold(
+            appBar: AppBar(title: Text('Joey\'s List App')),
+            body: Card(
+                child: Column(children: <Widget>[
+              Image.asset("assets/images/scarlet_spider.png"),
+              Text("Scarlet Spider")
+            ]))));
   }
 }
